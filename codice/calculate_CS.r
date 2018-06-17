7ac64372d7ed6cfa407c76ebe8a827457809e564
Alfonso <alfcrisci@gmail.com>
2018-06-17 23:12:56 +0200

    Update calculate_CS.r

--- codice/calculate_CS.r
+++ codice/calculate_CS.r
@@ -1,6 +1,5 @@
 library(raster)
 
-setwd("/home/alf/alf_github/UNIFI_PRIN_raster_data/ISPRA_CS")
 
 diff_CS=raster("ISPRA_CS_2015.tif")-raster("ISPRA_CS_2012.tif")
 writeRaster(diff_CS,"ISPRA_CS_diff_2015_2012.tif")
